c312292b5fbe6b10d426aa36ea0b7dc24f456018
wzsgptwj <118689897+wzsgptwj@users.noreply.github.com>
2024-09-23 18:24:43 +0100

    Add files via upload

---
+++ some_functions.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace hangman
+
+{
+    internal class some_functions
+    {
+        static Array check(string answer, string usrGuess)
+        {
+            List<Int32> positions = new List<Int32>();
+            positions.Add(-1);
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (System.Convert.ToChar(usrGuess) == System.Convert.ToChar(answer[i]))
+                {
+                    if (positions[0] == -1)
+                    {
+                        positions[0] = i;
+                    }
+                    else
+                    {
+                        positions.Add(i);
+                    }
+                }
+               
+            }
+
+            int[] positionsArray = positions.ToArray();
+            return positionsArray;
+            
+        }
+        static bool CheckIfDead(int NumOfWrongs){
+            if (NumOfWrongs >= 10)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(check("William","i"));
+            foreach(var item in check("william","w"))
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
